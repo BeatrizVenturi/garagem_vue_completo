dc4f9a484176365c3294facd4501cba1f653b6e7
BeatrizVenturi <venturibeatriz329@gmail.com>
2024-07-26 10:07:36 -0300

    commit inicial

---
+++ src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
+<template>
+  <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth app-id="r0Ee7CaXSSQoNEv99PqfsReI"></passage-auth>
+  </div>
+</template>
